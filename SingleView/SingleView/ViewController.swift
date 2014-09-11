610ad9bb379a89731f0cd7d4032dccfd99f5ab55
Alex Blewitt <alex.blewitt@gmail.com>
2014-09-11 21:23:59 +0100

    Chapter 3 - Adding items to the view

--- SingleView/SingleView/ViewController.swift
+++ SingleView/SingleView/ViewController.swift
@@ -26,14 +26,18 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		view.backgroundColor = UIColor.blackColor()
+		let label = UILabel(frame:view.bounds)
+		label.textColor = UIColor.whiteColor()
+		label.textAlignment = .Center
+		label.text = "Welcome to Swift"
+		view.addSubview(label)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
 
 }
 
