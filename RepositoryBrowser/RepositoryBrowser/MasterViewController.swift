611c7e15689713d91ad2e375eecc53fb97cb8ae1
Alex Blewitt <alex.blewitt@gmail.com>
2014-10-19 13:30:28 +0100

    Chapter 7 - Template RepositoryBrowser created

---
+++ RepositoryBrowser/RepositoryBrowser/MasterViewController.swift
@@ -0,0 +1,108 @@
+// Copyright (c) 2014, Alex Blewitt, Bandlem Ltd
+// Copyright (c) 2014, Packt Publishing Ltd
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+	var detailViewController: DetailViewController? = nil
+	var objects = NSMutableArray()
+
+
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
+		    self.clearsSelectionOnViewWillAppear = false
+		    self.preferredContentSize = CGSize(width: 320.0, height: 600.0)
+		}
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+		self.navigationItem.rightBarButtonItem = addButton
+		if let split = self.splitViewController {
+		    let controllers = split.viewControllers
+		    self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
+		}
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	func insertNewObject(sender: AnyObject) {
+		objects.insertObject(NSDate(), atIndex: 0)
+		let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+		self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+	}
+
+	// MARK: - Segues
+
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if segue.identifier == "showDetail" {
+		    if let indexPath = self.tableView.indexPathForSelectedRow() {
+		        let object = objects[indexPath.row] as NSDate
+		        let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+		        controller.detailItem = object
+		        controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+		        controller.navigationItem.leftItemsSupplementBackButton = true
+		    }
+		}
+	}
+
+	// MARK: - Table View
+
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return 1
+	}
+
+	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return objects.count
+	}
+
+	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+
+		let object = objects[indexPath.row] as NSDate
+		cell.textLabel.text = object.description
+		return cell
+	}
+
+	override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+		// Return false if you do not want the specified item to be editable.
+		return true
+	}
+
+	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+		if editingStyle == .Delete {
+		    objects.removeObjectAtIndex(indexPath.row)
+		    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+		} else if editingStyle == .Insert {
+		    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+		}
+	}
+
+
+}
+
