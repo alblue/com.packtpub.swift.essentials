3b4568287f1cfe204614152add1ec6aad40fadb9
Alex Blewitt <alex.blewitt@gmail.com>
2016-01-02 19:34:10 +0000

    Chapter 2 - QuickLook labels and colours

--- playing/MyPlayground.playground/Contents.swift
+++ playing/MyPlayground.playground/Contents.swift
@@ -9,3 +9,10 @@ for i in 1...12 {
 	let k = i
 	print("I is \(i)")
 }
+
+let blue = UIColor.blueColor()
+let size = CGRect(x:0,y:0,width:200,height:100)
+let label = UILabel(frame:size)
+label.text = str
+label.textColor = blue;
+label.font = UIFont.systemFontOfSize(24)
